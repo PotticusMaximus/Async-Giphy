3a35a72ead4fee38b2d2fbe1a5d5962429bd4e5b
PotticusMaximus <136069407+PotticusMaximus@users.noreply.github.com>
2023-10-30 12:59:36 +0000

    exercises done

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-require('dotenv').config();
+require("dotenv").config();
 
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+
+async function getImage(query) {
+  const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${this.query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+  try {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    console.log(data.data[0].url);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+console.log(process.env.API_KEY);
+getImage("cat");
